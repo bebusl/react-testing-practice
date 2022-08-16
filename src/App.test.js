46af714ec9c3ff8a5991e4963bd3044a060492fd
bebusl <bebus1998@naver.com>
2022-08-17 00:01:34 +0900

    feat: 단순한 static 테스팅 코드 작성

--- src/App.test.js
+++ src/App.test.js
@@ -19,8 +19,25 @@ test("the counter starts at 0", () => {
 });
 
 // 2. 마이너스 버튼이 올바른 텍스트를 렌더링하고 있는가
+
+test("minus button should showing -", () => {
+  render(<App />);
+  const minusButtonElement = screen.getByTestId("minus-button");
+  expect(minusButtonElement).toHaveTextContent("-");
+});
 // 3. 플러스 버튼이 올바른 텍스트를 렌더링하고 있는가
+
+test("plus button should showing +", () => {
+  render(<App />);
+  const plusButtonElement = screen.getByTestId("plus-button");
+  expect(plusButtonElement).toHaveTextContent("+");
+});
 // 4. +버튼을 누르면 카운터 숫자 1로 바뀜
 // 5. -버튼 누르면 카운터 숫자 -1로 바뀜
 // 6. on/off 버튼은 파란색인가?
+test("on/off button is blue", () => {
+  render(<App />);
+  const onOffButton = screen.getByTestId("on/off-button");
+  expect(onOffButton).toHaveStyle({ backgroundColor: "blue" });
+});
 // 7. on/off 버튼이 눌렸을 때 -,+버튼 disabled됐는지 확인
