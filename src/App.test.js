68b37925b60d7b162c4683843738ed391d22ed82
bebusl <bebus1998@naver.com>
2022-08-16 23:47:51 +0900

    feat: 테스트 상황 가정

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-
-  const lintTest = screen.getByRole("button", { name: "lintTest" });
-
-  //expect(lintTest.textContent).toBe("lintTest");
-  expect(lintTest).toHaveTextContent("lintTest");
-  //ESLint가 toBe매쳐보다 toHaveTextContent 매쳐 사용하는 것이 적절하다고 알려줌.
-});
+//test 상황
+// 1. 카운터 초기 : 0
+// 2. 마이너스 버튼이 올바른 텍스트를 렌더링하고 있는가
+// 3. 플러스 버튼이 올바른 텍스트를 렌더링하고 있는가
+// 4. +버튼을 누르면 카운터 숫자 1로 바뀜
+// 5. -버튼 누르면 카운터 숫자 -1로 바뀜
+// 6. on/off 버튼은 파란색인가?
+// 7. on/off 버튼이 눌렸을 때 -,+버튼 disabled됐는지 확인
