82089852a495ad0c170a9864864859b58801ce9e
이진희 <49019236+bebusl@users.noreply.github.com>
2023-04-03 16:14:14 +0900

    ondemand sunday icecream 디렉토리 구성

--- src/App.js
+++ src/App.js
@@ -1,46 +1,7 @@
-import { useState } from "react";
-import "./App.css";
-
-export function replaceCamelWithSpaces(colorName) {
-  return colorName.replace(/\B([A-Z])\B/g, " $1");
-}
+import React from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [disabled, setDisabled] = useState(false);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h3 data-testid="counter">{counter}</h3>
-        <div>
-          <button
-            data-testid="minus-button"
-            onClick={() => setCounter((count) => count - 1)}
-            disabled={disabled}
-          >
-            -
-          </button>
-          <button
-            data-testid="plus-button"
-            onClick={() => setCounter((count) => count + 1)}
-            disabled={disabled}
-          >
-            +
-          </button>
-        </div>
-        <div>
-          <button
-            data-testid="on/off-button"
-            style={{ backgroundColor: "blue" }}
-            onClick={() => setDisabled((prev) => !prev)}
-          >
-            on/off
-          </button>
-        </div>
-      </header>
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
