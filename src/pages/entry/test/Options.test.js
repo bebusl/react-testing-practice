3d2e4918b53c6911f1df695303321e94211d19a7
이진희 <49019236+bebusl@users.noreply.github.com>
2023-04-17 14:57:34 +0900

    find 쿼리를 이용해 비동기적으로 받아오는 데이터 렌더링 확인

--- src/pages/entry/test/Options.test.js
+++ src/pages/entry/test/Options.test.js
@@ -5,12 +5,13 @@ import Options from "../Options";
  * 서버에서 반환하는 이미지를 잘 보여주는지만 테스트
  */
 
-test("displays image for each scoop option from server", () => {
+test("displays image for each scoop option from server", async () => {
   render(<Options optionType="scoops" />);
 
   //find images
   //모든 이미지 이름은 scoop으로 끝나야 함
-  const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
+  // 이미지는 server에서 받아옴 => 비동기 => query도 비동기로 처리해주어야 함 => get/query대신 find를 써줘야 함
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
   //mutable 객체를 다룰 때는 toBe말고 toEqual사용해야 함
